bccff3766bf6bbbd2fa1a7dd1714552c787e1cec
limed <limed@mozilla.com>
2017-12-05 10:13:33 -0800

    Test prod upgrade

--- nubis/terraform/dns.tf
+++ nubis/terraform/dns.tf
@@ -8,13 +8,22 @@ resource "aws_route53_delegation_set" "haul-delegation" {
   reference_name = "${var.service_name}"
 }
 
+variable my_dns_zone {
+  type  = "map"
+
+  default = {
+    stage = "limed.me"
+    prod  = "sudoers.xyz"
+  }
+}
+
 module "sudoers_xyz" {
   source                  = "dns"
   region                  = "${var.region}"
   environment             = "${var.environment}"
   service_name            = "${var.service_name}"
 
-  zone_name               = "limed.me"
+  zone_name               = "${lookup(var.my_dns_zone, var.environment)}"
   route53_delegation_set  = "${aws_route53_delegation_set.haul-delegation.id}"
   elb_address             = "${module.load_balancer_web.address}"
 }
